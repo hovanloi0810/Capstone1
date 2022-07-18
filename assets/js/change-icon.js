e736e92e67bff439283a81b64649ff9b61316d9f
hovanloi0810 <hovanloi0810@gmail.com>
2022-07-18 13:17:33 +0700

    done

--- assets/js/change-icon.js
+++ assets/js/change-icon.js
@@ -1,5 +1,11 @@
 function changeIcon() {
     var icon = document.getElementById("myBtn");
-    icon.classList.toggle('fa-sun');
-    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('gg-sun');
+    icon.classList.toggle('gg-moon');
+}
+
+document.getElementById("btn-icon").onclick = function () {
+    var bar = document.getElementById("icon-respon");
+    bar.classList.toggle('fa-bars');
+    bar.classList.toggle('fa-xmark');
 }
